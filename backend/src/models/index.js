6d15c2f63caf57a77d82c0e8082a8fa2ad41f722
Sunil Kumar <102366208+sunil090@users.noreply.github.com>
2026-04-16 11:48:00 +0530

    fix

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV=production';
 const config = require('../config/config.js')[env];
 
 // Initialize sequelize with the provided config
